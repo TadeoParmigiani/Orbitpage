2f50bf2805cd4e61dd9e5d9fa6200fc39652c475
ezequielmartin03 <martinezequielnicolas@gmail.com>
2025-03-06 11:38:07 -0300

    first commit

---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,25 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Inicio",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Sobre Nosotros",
+    path: "/about",
+    newTab: false,
+  },
+  
+  {
+    id: 3,
+    title: "Contacto",
+    path: "/contact",
+    newTab: false,
+  },
+  
+];
+export default menuData;
